02996b47b6a093b4cb689621fda8d05d046857bf
jkwu <jkwu@amarsoft.com>
2020-08-03 22:55:12 +0800

    feat: 添加js常见特效，添加静态网页网站，添加async，await练习

---
+++ nodejs/md5.js
@@ -0,0 +1,24 @@
+/**
+ * Created by jkwu on 2019-11-27.
+ */
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  View,
+} from 'react-native';
+
+export default class md5 extends Component {
+  render() {
+    return (
+      < View
+    style = {
+    {
+      flex:1
+    }
+  }>
+  <
+    /View>
+  )
+    ;
+  }
+}
---
+++ svg/reg.js
@@ -0,0 +1,30 @@
+/**
+ * Created by jkwu on 2019-11-01.
+ */
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  View,
+} from 'react-native';
+
+export default class reg
+.
+js
+extends
+Component
+{
+  render()
+  {
+    return (
+      < View
+    style = {
+    {
+      flex:1
+    }
+  }>
+  <
+    /View>
+  )
+    ;
+  }
+}
